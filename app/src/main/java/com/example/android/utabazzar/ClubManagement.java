b2236f5aeece6c9f523244ec2c765d77c6cc51c2
Zachery Gentry <zacherygentry@gmail.com>
2018-04-14 18:33:00 -0500

    ChatMessage activity added

---
+++ app/src/main/java/com/example/android/utabazzar/ClubManagement.java
@@ -0,0 +1,16 @@
+package com.example.android.utabazzar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Date;
+
+public class ClubManagement extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_management);
+    }
+}
