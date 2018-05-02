67d19d45a5d16102b1f9870a448192eb294acac9
deviprasadtripathy <tripathy.devi7@gmail.com>
2018-05-02 17:38:38 -0500

    Presonal Chat added

--- app/src/main/java/com/example/android/utabazzar/ProductDetails1.java
+++ app/src/main/java/com/example/android/utabazzar/ProductDetails1.java
@@ -2,11 +2,8 @@ package com.example.android.utabazzar;
 
 import android.content.Intent;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.support.design.widget.FloatingActionButton;
-=======
 import android.content.Intent;
->>>>>>> 00499cb083ca2e7577f298cd3864db7a7f049dcc
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -23,13 +20,10 @@ public class ProductDetails1 extends AppCompatActivity {
     ImageView imageView;
     TextView producName, productPrice, productId, seller_name, seller_phone, seller_email, seller_block, seller_room, time_period;
     String prod_id;
-<<<<<<< HEAD
     public static String seller_email_stat, product_name_stat, seller_utaid_stat;
-=======
 
     public static String productPriceUsd;
 
->>>>>>> 00499cb083ca2e7577f298cd3864db7a7f049dcc
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,7 +51,6 @@ public class ProductDetails1 extends AppCompatActivity {
         producName.setText(album.getProduct_name());
         productPrice.setText("$"+album.getProduct_price());
         seller_name.setText(album.getSeller_name());
-<<<<<<< HEAD
         seller_email_stat = album.getSeller_email();
         seller_utaid_stat = album.getSeller_block();
         product_name_stat = album.getProduct_name();
@@ -70,9 +63,7 @@ public class ProductDetails1 extends AppCompatActivity {
                 finish();
             }
         });
-=======
         productPriceUsd = album.getProduct_price();
->>>>>>> 00499cb083ca2e7577f298cd3864db7a7f049dcc
         //productId.setText(album.getProduct_id());
         //seller_phone.setText(album.getSeller_phone());
         //seller_email.setText(album.getSeller_email());
